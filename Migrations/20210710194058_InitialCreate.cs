8616847afa28f9968ddf93d6a8cc8a800f6d67dc
Guilherme Ribeiro <guilhemerps94@gmail.com>
2021-07-10 16:52:59 -0300

    Novos ajustes para criação do banco

---
+++ Migrations/20210710194058_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webDotNet.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Valor = table.Column<double>(nullable: false),
+                    DataPagamento = table.Column<DateTime>(nullable: false),
+                    Descriacao = table.Column<string>(maxLength: 500, nullable: true),
+                    ObraAssociada = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+        }
+    }
+}
